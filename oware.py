7db7a3f17b00a68c6ae3c7519d9c27f593ee82c0
Eli Dey <stresslevelxer0@gmail.com>
2020-05-25 10:27:35 -0400

    Seeing if this works

---
+++ oware.py
@@ -0,0 +1,9 @@
+
+def intro():
+    print("Hello! Welcome to the game of Oware!")
+    print("Would you like to start the tutorial? (Y/N)")
+    tanswer = input() #Does the player want to start the tutorial?
+    print(tutanser)
+
+if __name__ == "__main__":
+    intro()
