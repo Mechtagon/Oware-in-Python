3b1fdb88ef00bd055c227332cbb15026e2e0d036
Eli Dey <stresslevelxer0@gmail.com>
2020-05-25 11:40:26 -0400

    Added the intro for the game

--- oware.py
+++ oware.py
@@ -2,9 +2,27 @@
 def intro():
     print("Hello! Welcome to the game of Oware!")
     print("Would you like to start the tutorial? (Y/N)")
-    tanswer = input() #Does the player want to start the tutorial?
-    print(tutanser)
-    "Getting the hang of it I think"
+    tanswer = input().upper() #Does the player want to start the tutorial?
+    
+    if tanswer == "N":
+        print("You have selected NO")
+    
+    elif tanswer == "Y":
+        print("You have selected YES")
+    
+    else:
+        while tanswer != "Y" and tanswer != "N":
+            print("That is not a valid answer, try again")
+            tanswer = input().upper()
+            
+            if tanswer == "N":
+                print("You have selected NO")
+            
+            elif tanswer == "Y":
+                print("You have selected YES")
+                
+def printboard():
+    
 
 if __name__ == "__main__":
     intro()
